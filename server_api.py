44c1bde369173bcb9cdfadf7511e4e83f6fa8ae7
amanzano <manzanoa16>
2019-11-04 22:20:34 -0300

    icon

--- server_api.py
+++ server_api.py
@@ -24,7 +24,7 @@ app = web.application(urls, globals())
 dna_expert = DNAExpert()
 
 class icon:
-    def GET(self): raise web.seeother('magneto.jpg')
+    def GET(self): raise web.seeother('magneto.ico')
 
 
 class Mutant:
