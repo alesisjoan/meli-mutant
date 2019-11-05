97ca16ad5a6530b0e3bc37e9613c646676b4b69d
amanzano <manzanoa16>
2019-11-04 22:09:19 -0300

    servidores unificados

--- server_api.py
+++ server_api.py
@@ -16,6 +16,7 @@ web.config.debug = False
 
 urls = (
     '/mutant', 'Mutant',
+    '/stats', 'Stats',
 )
 app = web.application(urls, globals())
 
@@ -37,14 +38,34 @@ class Mutant:
             return web.HTTPError('400 Bad Request')
 
 
-class DNAController(web.application):
+class Stats:
+
+    def GET(self):
+        count_human_dna = 0
+        count_mutant_dna = 0
+        ratio = 0
+        for key in r.keys():
+            result = r.get(key)
+            count_human_dna += not int(result)
+            count_mutant_dna += int(result)
+        if count_mutant_dna and count_human_dna:
+           ratio = round(float(count_mutant_dna / count_human_dna), 2)
+        return {
+            'count_mutant_dna': count_mutant_dna,
+            'count_human_dna': count_human_dna,
+            'ratio': ratio
+        }
+
+
+
+class Controller(web.application):
     def run(self, port=8081, *middleware):
         func = self.wsgifunc(*middleware)
         return web.httpserver.runsimple(func, ('0.0.0.0', port))
 
 
 def run():
-    app = DNAController(urls, globals())
+    app = Controller(urls, globals())
     port = os.environ.get("PORT")
     app.run(int(port) if port else 8081)
 
